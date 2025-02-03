11d14d259c285607c0116f4625725bd714f6116b
Rony Bormon Nirob <ronybormon092@gmail.com>
2025-02-04 02:16:57 +0600

    Day 1

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// ///////// constructor function
+
+// function biskitOpadan(color) {
+//   this.width = 12;
+//   this.height = 12;
+//   this.color = color;
+//   this.shape = "circle";
+// }
+
+// var bis1 = new biskitOpadan("red");
+// var bis2 = new biskitOpadan("green");
+
+// //////////////exampel
+
+
+// function button(color, Size) {
+//   this.width = 12;
+//   this.height = 12;
+//   this.redius =20;
+//   this.color = color;
+//   this.Size = Size;
+// }
+
+// var buttonstyle1 = new button("red", "20");
+// var buttonstyle2 = new button("green", "30");
+// var buttonstyle3 = new button("black","40");
+
+
+
+// //////////// frist class function
+
+// function abcd(){
+
+// }
+
+// var abcd = function (){
+
+// }
+
+
+
+////////// New keyword is blnk object 
+
+
+
+
+
+
+
